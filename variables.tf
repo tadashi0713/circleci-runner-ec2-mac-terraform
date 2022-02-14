b2f735e5cd4da3f543e1f5006671672c854268d3
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-02-15 08:25:12 +0900

    Comment out

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "aws_region" {}
 variable "aws_availability_zone" {}
 variable "subnet_id" {}
+
+variable "cf_stack_name" {
+  description = "Dedicated host CloudFormation stack name. It can include letters (A-Z and a-z), numbers (0-9), and dashes (-)."
+  type        = string
+  default     = "host-resource-group"
+}
+
+variable "host_resource_group_prefix" {
+  description = "Prefix used to create ASG Launch template & Host Resource Group license configuration"
+  type        = string
+  default     = "mac1-"
+}

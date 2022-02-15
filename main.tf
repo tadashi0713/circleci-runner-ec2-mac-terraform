378a91807604766cf00d38975a01ff92fbe4627c
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-02-15 20:38:29 +0900

    Remove comment

--- main.tf
+++ main.tf
@@ -87,65 +87,6 @@ resource "aws_cloudformation_stack" "mac1_host_resource_group" {
 STACK
 }
 
-// resource "aws_ec2_host" "mac" {
-//   instance_type     = "mac1.metal"
-//   availability_zone = var.aws_availability_zone
-//   auto_placement    = "on"
-
-//   tags = {
-//     Name = "Tadashi Terraform Mac"
-//   }
-// }
-
-// resource "aws_ec2_tag" "mac" {
-//   resource_id = aws_ec2_host.mac.id
-//   key         = "Name"
-//   value       = "Tadashi Terraform Mac"
-// }
-
-// resource "aws_instance" "mac" {
-//   ami           = data.aws_ami.mac.id
-//   instance_type = "mac1.metal"
-//   host_id       = aws_ec2_host.mac.id
-//   subnet_id     = var.subnet_id # Subnet ID in the same AZ as the dedicated host
-//   user_data = base64encode(
-//     templatefile(
-//       "${path.module}/userdata/runner_install.sh.tpl",
-//       {
-//         auth_token  = "Hello, this is script"
-//       }
-//     )
-//   )
-
-//   tags = {
-//     Name = "Tadashi Terraform Mac"
-//   }
-// }
-
-// data "aws_ami" "mac" {
-//   most_recent = true
-
-//   filter {
-//     name   = "name"
-//     values = ["amzn-ec2-macos-11.6*"]
-//   }
-
-//   filter {
-//     name   = "virtualization-type"
-//     values = ["hvm"]
-//   }
-
-//   owners = ["amazon"]
-// }
-
-// output "mac_ami" {
-//   value = data.aws_ami.mac.id
-// }
-
-// output "dedicated-host" {
-//   value = aws_ec2_host.mac.id
-// }
-
 resource "random_pet" "mac_workers" {
   length    = 2
   prefix    = var.worker_prefix
